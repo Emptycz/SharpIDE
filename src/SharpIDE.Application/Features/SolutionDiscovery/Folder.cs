614893bf1ac998bcb78d6c5a44d02e637db56624
Matt Parker [SSW] <61717342+MattParkerDev@users.noreply.github.com>
2025-01-10 19:52:28 +1000

    add stuff

---
+++ src/SharpIDE.Application/Features/SolutionDiscovery/Folder.cs
@@ -0,0 +1,13 @@
+﻿namespace SharpIDE.Application.Features.SolutionDiscovery;
+
+public class Folder
+{
+	public required string Name { get; set; }
+	public List<Folder> Folders { get; set; } = [];
+	public List<MyFile> Files { get; set; } = [];
+}
+
+public class MyFile
+{
+	public required string Name { get; set; }
+}
