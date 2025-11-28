a62e1710cf8cc10278c7321a1907465ab0a79709
Matt Parker <61717342+MattParkerDev@users.noreply.github.com>
2025-11-28 23:58:57 +1000

    add txt icon

--- src/SharpIDE.Godot/Features/SolutionExplorer/SolutionExplorerPanel.FileIcons.cs
+++ src/SharpIDE.Godot/Features/SolutionExplorer/SolutionExplorerPanel.FileIcons.cs
@@ -9,6 +9,7 @@ public partial class SolutionExplorerPanel
     private readonly Texture2D _razorIcon = ResourceLoader.Load<Texture2D>("uid://cff7jlvj2tlg2");
     private readonly Texture2D _jsonIcon = ResourceLoader.Load<Texture2D>("uid://csrwpjk77r731");
     private readonly Texture2D _jsIcon = ResourceLoader.Load<Texture2D>("uid://cpdobpjrm2suc");
+    private readonly Texture2D _txtIcon = ResourceLoader.Load<Texture2D>("uid://b6bpjhs2o1j2l");
 
     private Texture2D GetIconForFileExtension(string fileExtension)
     {
@@ -18,6 +19,7 @@ public partial class SolutionExplorerPanel
             ".razor" or ".cshtml" => _razorIcon,
             ".json" => _jsonIcon,
             ".js" => _jsIcon,
+            ".txt" => _txtIcon,
             _ => _csIcon
         };    
         return texture;
